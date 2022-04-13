b8466a264aeeb83594d3107fb14b076dffce63c3
Timothy Morton <wookie-hunter@hotmail.com>
2022-04-13 14:12:19 +1200

    Add project files.

---
+++ Updater/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+
+using Microsoft.Extensions.Configuration;
+
+Console.WriteLine("Update Starting Soon...");
+if(Test())Environment.Exit(0);
+
+bool Test()
+{
+    if (!File.Exists("appsettings.json")) throw new Exception("appsettings not found");
+    var a = AppSet("UpdateFrom");
+    Console.WriteLine(a);
+    return true;
+}
+
+static string AppSet(string key)
+{
+    IConfiguration configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+    return configuration[key];
+}
