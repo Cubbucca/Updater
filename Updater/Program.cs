4a5dc95f93c1a854c30e61b3e8022706e34d8c68
Timothy Morton <wookie-hunter@hotmail.com>
2022-04-13 15:36:39 +1200

    almost working

--- Updater/Program.cs
+++ Updater/Program.cs
@@ -2,16 +2,46 @@
 
 
 using Microsoft.Extensions.Configuration;
-
+using System.Diagnostics;
 Console.WriteLine("Update Starting Soon...");
-if(Test())Environment.Exit(0);
+if(Update())Environment.Exit(0);
 
-bool Test()
+bool Update()
 {
+    var currentDir = Environment.CurrentDirectory;
     if (!File.Exists("appsettings.json")) throw new Exception("appsettings not found");
-    var a = AppSet("UpdateFrom");
-    Console.WriteLine(a);
+    Console.WriteLine("Settings Loading...");
+    var target = AppSet("ExeName");
+    Console.WriteLine(target);
+    var source = AppSet("UpdateFrom");
+    Console.WriteLine(source);
+    Console.WriteLine("Checking File Versions ...");
+    if (VersionMatch($"{source}{target}", target)) return true;
+    CopyFolderContents(source, currentDir,"*.*", true, true);
     return true;
+}
+
+bool VersionMatch(string source, string target)
+{
+    var filepath = source;
+    if (!File.Exists(target))
+    {
+        Console.WriteLine($"No Local Exe, Updated Will Now Install It");
+        return false;
+    }
+    if (File.Exists(filepath))
+    {
+        var updateinfo = FileVersionInfo.GetVersionInfo(filepath).FileVersion;
+        var currentinfo = FileVersionInfo.GetVersionInfo(target).FileVersion;
+        if (updateinfo != currentinfo)
+        {
+            Console.WriteLine($"Version miss match current:{currentinfo} @source:{updateinfo}");
+            return false;
+        }
+        Console.WriteLine($"{currentinfo} is already latest Version");
+        return true;
+    }
+    throw new Exception("source can not be read");
 }
 
 static string AppSet(string key)
@@ -20,4 +50,43 @@ static string AppSet(string key)
                     .AddJsonFile("appsettings.json")
                     .Build();
     return configuration[key];
+}
+
+void CopyFolderContents(string sourceFolder, string destinationFolder, string mask, Boolean createFolders, Boolean recurseFolders)
+{
+    try
+    {
+        if (!sourceFolder.EndsWith(@"\")) { sourceFolder += @"\"; }
+        if (!destinationFolder.EndsWith(@"\")) { destinationFolder += @"\"; }
+
+        var exDir = sourceFolder;
+        var dir = new DirectoryInfo(exDir);
+        SearchOption so = (recurseFolders ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly);
+
+        foreach (string sourceFile in Directory.GetFiles(dir.ToString(), mask, so))
+        {
+            FileInfo srcFile = new FileInfo(sourceFile);
+            string srcFileName = srcFile.Name;
+            if (srcFileName == "appsettings.json") continue;
+            Console.WriteLine($"Checking - {srcFileName}");
+            // Create a destination that matches the source structure
+            FileInfo destFile = new FileInfo(destinationFolder + srcFile.FullName.Replace(sourceFolder, ""));
+
+            if (!Directory.Exists(destFile.DirectoryName) && createFolders)
+            {
+                Console.WriteLine($"Creating Dir - {destFile.DirectoryName}");
+                Directory.CreateDirectory(destFile.DirectoryName);
+            }
+
+            if (srcFile.LastWriteTime > destFile.LastWriteTime || !destFile.Exists)
+            {
+                Console.WriteLine($"Copying File - {destFile.FullName}");
+                File.Copy(srcFile.FullName, destFile.FullName, true);
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        System.Diagnostics.Debug.WriteLine(ex.Message + Environment.NewLine + Environment.NewLine + ex.StackTrace);
+    }
 }
